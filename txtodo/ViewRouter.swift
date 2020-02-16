8aa1463d771b68be2fe696ac61d6cc16fb184494
therealFIGBERT <figbertwelner@gmail.com>
2020-02-15 21:21:58 -0800

    :sparkles: Add independent transition (without NavigationView)

---
+++ txtodo/ViewRouter.swift
@@ -0,0 +1,14 @@
+//
+//  ViewRouter.swift
+//  txtodo
+//
+//  Created by FIGBERT on 2/15/20.
+//  Copyright © 2020 FIGBERT Industries. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+
+class ViewRouter: ObservableObject {
+    @Published var currentPage: String = "intro"
+}
