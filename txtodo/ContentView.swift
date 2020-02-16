9d275fc9c2079cb6fa28ea55daafbb4c705b3a33
therealFIGBERT <figbertwelner@gmail.com>
2020-02-15 17:07:23 -0800

    Initial commit

---
+++ txtodo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  txtodo
+//
+//  Created by FIGBERT on 2/15/20.
+//  Copyright © 2020 FIGBERT Industries. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
