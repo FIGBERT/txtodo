46ebe542cd81af9979482e7c6e5d0a69b6daf336
therealFIGBERT <figbertwelner@gmail.com>
2020-02-23 08:05:13 -0800

    :sparkles: Add custom alert with a TextField

---
+++ txtodo/TextFieldAlert.swift
@@ -0,0 +1,59 @@
+//
+//  TextFieldAlert.swift
+//  txtodo
+//
+//  Created by Benjamin Welner on 2/23/20.
+//  Copyright © 2020 FIGBERT Industries. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+
+struct TextFieldAlert<Presenting>: View where Presenting: View {
+    @Binding var isShowing: Bool
+    @Binding var text: String
+    let presenting: Presenting
+    let title: String
+
+    var body: some View {
+        GeometryReader { (deviceSize: GeometryProxy) in
+            ZStack {
+                self.presenting
+                    .disabled(self.isShowing)
+                VStack {
+                    Text(self.title)
+                    TextField("priority", text: self.$text)
+                    Divider()
+                    HStack {
+                        Button(action: {
+                            withAnimation {
+                                self.isShowing.toggle()
+                            }
+                        }) {
+                            Text("dismiss")
+                        }
+                    }
+                }
+                .padding()
+                .background(Color.white)
+                .frame(
+                    width: deviceSize.size.width*0.7,
+                    height: deviceSize.size.height*0.7
+                )
+                .shadow(radius: CGFloat.init(1))
+                .opacity(self.isShowing ? 1.0 : 0.0)
+            }
+        }
+    }
+}
+
+extension View {
+    func textFieldAlert(isShowing: Binding<Bool>,
+                        text: Binding<String>,
+                        title: String) -> some View {
+        TextFieldAlert(isShowing: isShowing,
+                       text: text,
+                       presenting: self,
+                       title: title)
+    }
+}
