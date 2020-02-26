e33dce7ca61205e329d0e14efaee340aae957f4c
therealFIGBERT <figbertwelner@gmail.com>
2020-02-26 13:16:39 -0800

    :sparkles: Add ability to create noteTasks on the homepage

--- txtodo/Task.swift
+++ txtodo/Task.swift
@@ -40,6 +40,84 @@ struct taskNote: View {
     }
 }
 
+struct addNoteTask: View {
+    @EnvironmentObject var globalVars: GlobalVars
+    @State var addingTask: Bool = false
+    @State var newTaskText: String = ""
+    @State var newTaskPriority: Int = 0
+    var body: some View {
+        Group {
+            if !addingTask {
+                Button(action: {
+                    self.addingTask = true
+                }) {
+                    HStack {
+                        Image(systemName: "plus.square")
+                            .font(.system(size: 20, weight: .light))
+                            .foregroundColor(Color.init(UIColor.systemGray))
+                        Spacer()
+                        Text("create a task")
+                            .font(.system(size: 20, weight: .light))
+                            .foregroundColor(Color.init(UIColor.systemGray))
+                        Spacer()
+                        Image(systemName: "plus.square")
+                            .font(.system(size: 20, weight: .light))
+                            .foregroundColor(Color.init(UIColor.systemGray))
+                    }.padding(.horizontal, 25)
+                }
+            } else {
+                HStack {
+                    Button(action: {
+                        self.newTaskText = ""
+                        self.newTaskPriority = 0
+                        self.addingTask = false
+                    }) {
+                        Image(systemName: "multiply.square")
+                            .font(.system(size: 20, weight: .light))
+                            .foregroundColor(Color.init(UIColor.systemGray))
+                    }
+                    Spacer()
+                    TextField("new task", text: $newTaskText)
+                        .font(.system(size: 20, weight: .light))
+                        .foregroundColor(Color.init(UIColor.systemGray))
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .multilineTextAlignment(.center)
+                    Picker(
+                        selection: $newTaskPriority,
+                        label: Text("task priority"),
+                        content: {
+                            Text("0").tag(0)
+                            Text("1").tag(1)
+                            Text("2").tag(2)
+                            Text("3").tag(3)
+                    })
+                        .pickerStyle(SegmentedPickerStyle())
+                    Spacer()
+                    Button(action: {
+                        print(self.newTaskPriority)
+                        print(self.newTaskText)
+                        self.globalVars.dailyTasks.append(
+                            noteTask(
+                                main: task(
+                                    text: self.newTaskText,
+                                    priority: self.newTaskPriority
+                                )
+                            )
+                        )
+                        self.newTaskText = ""
+                        self.newTaskPriority = 0
+                        self.addingTask = false
+                    }) {
+                        Image(systemName: "plus.square")
+                            .font(.system(size: 20, weight: .light))
+                            .foregroundColor(Color.init(UIColor.systemGray))
+                    }
+                }.padding(.horizontal, 25)
+            }
+        }
+    }
+}
+
 struct noteTaskView: View {
     @State var task_: noteTask
     let calendar = Calendar.current
@@ -71,6 +149,9 @@ struct noteTaskView: View {
                     .font(.system(size: 10, weight: .light))
             } else if task_.main.priority == 3 {
                 Text("! ! !")
+                    .font(.system(size: 10, weight: .light))
+            } else {
+                Text("     ")
                     .font(.system(size: 10, weight: .light))
             }
         }.padding(.horizontal, 25)
