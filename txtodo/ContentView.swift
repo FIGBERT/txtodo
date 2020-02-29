ed4e4d359f51ac6017123cdfd58616163267f0c9
therealFIGBERT <figbertwelner@gmail.com>
2020-02-28 17:58:03 -0800

    Add ability to add notes to daily tasks, sign application to personal team

--- txtodo/ContentView.swift
+++ txtodo/ContentView.swift
@@ -45,7 +45,7 @@ struct HomeView: View {
                             .font(.system(size: 25, weight: .medium, design: .rounded))
                             .underline()
                         ForEach(self.globalVars.dailyTasks.indices, id: \.self) { index in
-                            noteTaskView(task_: self.globalVars.dailyTasks[index])
+                            noteTaskView(taskIndex: index)
                         }
                         addTask(createType: "daily")
                     }
