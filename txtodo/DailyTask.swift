3b9d4f4863846aa1503ed800cdbe36ed66fa432b
therealFIGBERT <figbertwelner@gmail.com>
2020-03-07 22:44:39 -0800

    :sparkles: Add ability to delete daily tasks

--- txtodo/DailyTask.swift
+++ txtodo/DailyTask.swift
@@ -13,27 +13,44 @@ struct dailyTaskView: View {
     @EnvironmentObject var globalVars: GlobalVars
     @State var editing: Bool = false
     @State var viewingNotes: Bool = false
+    @State var confirmingDelete: Bool = false
+    @State var deleted: Bool = false
     let taskIndex: Int
     var body: some View {
         HStack {
-            Button(action: {
-                self.globalVars.dailyTasks[self.taskIndex].main.complete.toggle()
-            }) {
-                if globalVars.dailyTasks[taskIndex].main.complete {
-                    Image(systemName: "checkmark.square")
-                } else {
-                    Image(systemName: "square")
+            if !deleted {
+                Button(action: {
+                    self.globalVars.dailyTasks[self.taskIndex].main.complete.toggle()
+                }) {
+                    if globalVars.dailyTasks[taskIndex].main.complete {
+                        Image(systemName: "checkmark.square")
+                            .font(.system(size: 25, weight: .light))
+                            .foregroundColor(Color.init(UIColor.label))
+                    } else {
+                        Image(systemName: "square")
+                            .font(.system(size: 25, weight: .light))
+                            .foregroundColor(Color.init(UIColor.label))
+                    }
                 }
+            } else {
+                Image(systemName: "square")
+                    .font(.system(size: 25, weight: .light))
+                    .foregroundColor(Color.init(UIColor.label))
             }
-                .font(.system(size: 25, weight: .light))
-                .foregroundColor(Color.init(UIColor.label))
             Spacer()
-            if !editing {
+            if deleted {
+                Text("deleting...")
+                    .font(.system(size: 20, weight: .light))
+                    .foregroundColor(Color.init(UIColor.label))
+            } else if !editing {
                 Text(globalVars.dailyTasks[taskIndex].main.text)
                     .font(.system(size: 20, weight: .light))
                     .foregroundColor(Color.init(UIColor.label))
                     .onTapGesture(count: 2) {
                         self.editing = true
+                    }
+                    .onLongPressGesture {
+                        self.confirmingDelete = true
                     }
                     .onTapGesture(count: 1) {
                         self.viewingNotes = true
@@ -52,19 +69,36 @@ struct dailyTaskView: View {
                     .multilineTextAlignment(.center)
             }
             Spacer()
-            if globalVars.dailyTasks[taskIndex].main.priority == 1 {
-                Text("  !  ")
-                    .font(.system(size: 10, weight: .light))
-            } else if globalVars.dailyTasks[taskIndex].main.priority == 2 {
-                Text(" ! ! ")
-                    .font(.system(size: 10, weight: .light))
-            } else if globalVars.dailyTasks[taskIndex].main.priority == 3 {
-                Text("! ! !")
-                    .font(.system(size: 10, weight: .light))
+            if !deleted {
+                if globalVars.dailyTasks[taskIndex].main.priority == 1 {
+                    Text("  !  ")
+                        .font(.system(size: 10, weight: .light))
+                } else if globalVars.dailyTasks[taskIndex].main.priority == 2 {
+                    Text(" ! ! ")
+                        .font(.system(size: 10, weight: .light))
+                } else if globalVars.dailyTasks[taskIndex].main.priority == 3 {
+                    Text("! ! !")
+                        .font(.system(size: 10, weight: .light))
+                } else {
+                    Text("     ")
+                        .font(.system(size: 10, weight: .light))
+                }
             } else {
                 Text("     ")
                     .font(.system(size: 10, weight: .light))
             }
-        }.padding(.horizontal, 25)
+        }
+            .padding(.horizontal, 25)
+            .alert(isPresented: $confirmingDelete) {
+                Alert(
+                    title: Text("confirm delete"),
+                    message: Text("the task will be gone forever, with no option to restore"),
+                    primaryButton: .destructive(Text("delete")) {
+                        self.globalVars.dailyTasks.remove(at: self.taskIndex)
+                        self.deleted = true
+                    },
+                    secondaryButton: .cancel(Text("cancel"))
+                )
+            }
     }
 }
