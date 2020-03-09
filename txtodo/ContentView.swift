fce2c4d6f764a73cd6304f8b76dedcfa9d2fac61
therealFIGBERT <figbertwelner@gmail.com>
2020-03-08 18:01:37 -0700

    Remove extraneous ZStack from the home view

--- txtodo/ContentView.swift
+++ txtodo/ContentView.swift
@@ -11,37 +11,33 @@ import SwiftUI
 struct ContentView: View {
     @EnvironmentObject var globalVars: GlobalVars
     var body: some View {
-        ZStack {
-            Color.init(UIColor.systemGray6)
-                .edgesIgnoringSafeArea(.all)
-            ScrollView(.vertical, showsIndicators: false) {
-                VStack {
-                    Text("floating")
-                        .font(.system(size: 25, weight: .medium, design: .rounded))
-                        .underline()
-                    ForEach(self.globalVars.floatingTasks.indices, id: \.self) { index in
-                        superTaskView(taskIndex: index)
-                    }
-                    if globalVars.floatingTasks.count < 3 {
-                        addTask(createType: "floating")
-                    }
+        ScrollView(.vertical, showsIndicators: false) {
+            VStack {
+                Text("floating")
+                    .font(.system(size: 25, weight: .medium, design: .rounded))
+                    .underline()
+                ForEach(self.globalVars.floatingTasks.indices, id: \.self) { index in
+                    superTaskView(taskIndex: index)
                 }
-                    .padding(.top, 60)
-                    .padding(.bottom, 45)
-                VStack {
-                    Text("today")
-                        .font(.system(size: 25, weight: .medium, design: .rounded))
-                        .underline()
-                    ForEach(self.globalVars.dailyTasks.indices, id: \.self) { index in
-                        dailyTaskView(taskIndex: index)
-                    }
-                    addTask(createType: "daily")
+                if globalVars.floatingTasks.count < 3 {
+                    addTask(createType: "floating")
                 }
-                Spacer()
             }
-                .background(Color.init(UIColor.systemGray6)
-                .edgesIgnoringSafeArea(.all))
+                .padding(.top, 60)
+                .padding(.bottom, 45)
+            VStack {
+                Text("today")
+                    .font(.system(size: 25, weight: .medium, design: .rounded))
+                    .underline()
+                ForEach(self.globalVars.dailyTasks.indices, id: \.self) { index in
+                    dailyTaskView(taskIndex: index)
+                }
+                addTask(createType: "daily")
+            }
+            Spacer()
         }
+            .background(Color.init(UIColor.systemGray6)
+            .edgesIgnoringSafeArea(.all))
     }
 }
 
