65694177e9da4fbfaf6247971ad9737464d18e04
therealFIGBERT <figbertwelner@gmail.com>
2020-03-14 21:49:12 -0700

    Add dailyTask and dailyTaskNote permanence using Core Data, breaking the superTask system
    The previous system used a global EnvironmentObject to create lists of noteTask and superTask objects. This has been replaced with a Core Data "NoteTask" class, identical to the "noteTask" struct with a few minor changes (priority is stored as Int16, NSManagedObjects, etc), which is updated with quick, asynchronus requests to the phone's storage infrastructure. This allows created tasks and notes to be kept between uses of the application – a fairly standard, very important feature.
    
    The superTask system needs to be reworked to function within the Core Data system. Previously, storing an array of noteTasks within a superTask was no large hassle. However, Core Data "relationships" are much more complicated. This feature will be revisited in the future, and included in the final release, though will not be in active development until after the release of the first alpha. The current plan is to iron out all bugs within the current Core Data infrastructure, introduce time-based clearing of Daily Tasks, and release the app for alpha testers.

---
+++ txtodo/Data Models/NoteTask+CoreDataProperties.swift
@@ -0,0 +1,26 @@
+//
+//  NoteTask+CoreDataProperties.swift
+//  txtodo
+//
+//  Created by FIGBERT on 3/13/20.
+//  Copyright © 2020 FIGBERT Industries. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension NoteTask {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<NoteTask> {
+        return NSFetchRequest<NoteTask>(entityName: "NoteTask")
+    }
+
+    @NSManaged public var completed: Bool
+    @NSManaged public var priority: Int16
+    @NSManaged public var name: String
+    @NSManaged public var notes: Array<String>
+    @NSManaged public var id: UUID
+
+}
