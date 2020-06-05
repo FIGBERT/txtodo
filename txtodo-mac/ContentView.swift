2c977b415c4cd8303c9862ca3c113fcc41bb049e
therealFIGBERT <figbert@figbert.com>
2020-06-04 23:48:57 -0700

    Add MacOS menu bar application template
    The new txtodo-mac folder will contain the MacOS companion app, which will use the newly implemented CloudKit system to sync tasks between devices.
    
    As of right now, the app syncs its core data storage to the CloudKit database, but it does not yet have an interface (that's coming tomorrow) – currenly, it just says "Hello world."

---
+++ txtodo-mac/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  txtodo-mac
+//
+//  Created by FIGBERT on 6/4/20.
+//  Copyright © 2020 FIGBERT Industries. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
