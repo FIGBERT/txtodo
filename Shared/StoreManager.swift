70fafb5cc5b0c57b045c79fca56c71ceba2fb56c
FIGBERT <figbert@figbert.com>
2020-11-21 14:19:25 -0800

    Add StoreManager.swift to manage in-app purchases

---
+++ Shared/StoreManager.swift
@@ -0,0 +1,88 @@
+//
+//  StoreManager.swift
+//  txtodo
+//
+//  Created by FIGBERT on 11/20/20.
+//
+
+import Foundation
+import StoreKit
+
+class StoreManager: NSObject, ObservableObject, SKProductsRequestDelegate, SKPaymentTransactionObserver {
+    
+    @Published var myProducts = [SKProduct]()
+    @Published var transactionState: SKPaymentTransactionState?
+    var request: SKProductsRequest!
+    
+    func getProducts(productIDs: [String]) {
+        let request = SKProductsRequest(productIdentifiers: Set(productIDs))
+        request.delegate = self
+        request.start()
+    }
+    
+    func productsRequest(_ request: SKProductsRequest, didReceive response: SKProductsResponse) {
+        if !response.products.isEmpty {
+            for fetchedProduct in response.products {
+                DispatchQueue.main.async {
+                    self.myProducts.append(fetchedProduct)
+                }
+            }
+        }
+        
+        for invalidIdentifier in response.invalidProductIdentifiers {
+            print("Invalid identifiers found: \(invalidIdentifier)")
+        }
+    }
+    
+    func request(_ request: SKRequest, didFailWithError error: Error) {
+        print("Request did fail: \(error)")
+    }
+    
+    func purchaseProduct(product: SKProduct) {
+        if SKPaymentQueue.canMakePayments() {
+            let payment = SKPayment(product: product)
+            SKPaymentQueue.default().add(payment)
+        } else {
+            print("User can't make payment.")
+        }
+    }
+    
+    func paymentQueue(_ queue: SKPaymentQueue, updatedTransactions transactions: [SKPaymentTransaction]) {
+        for transaction in transactions {
+            switch transaction.transactionState {
+            case .purchasing:
+                transactionState = .purchasing
+            case .purchased:
+                UserDefaults.standard.setValue(true, forKey: transaction.payment.productIdentifier)
+                queue.finishTransaction(transaction)
+                transactionState = .purchased
+            case .restored:
+                UserDefaults.standard.setValue(true, forKey: transaction.payment.productIdentifier)
+                queue.finishTransaction(transaction)
+                transactionState = .restored
+            case .failed, .deferred:
+                print("Payment Queue Error: \(String(describing: transaction.error))")
+                queue.finishTransaction(transaction)
+                transactionState = .failed
+            default:
+                queue.finishTransaction(transaction)
+            }
+        }
+    }
+    
+    func restoreProducts() {
+        SKPaymentQueue.default().restoreCompletedTransactions()
+    }
+}
+
+extension SKProduct {
+    func priceFormatted() -> String {
+        let roundedPrice = self.price.doubleValue.rounded()
+        let formatter = NumberFormatter()
+        formatter.locale = self.priceLocale
+        formatter.minimumFractionDigits = 0
+        formatter.maximumFractionDigits = String(roundedPrice).contains(".00") ? 0 : 2
+        formatter.numberStyle = .currency
+        return formatter.string(from: NSNumber(value: roundedPrice))!
+    }
+}
